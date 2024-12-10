c59161900280339bf45327ef9cb1a605a5c8eb72
api-scraping <bao.francis.nguyen@gmail.com>
2024-12-10 23:58:32 +0100

    modifié

--- Desktop/BTC_ANALYSER/v1.py
+++ Desktop/BTC_ANALYSER/v1.py
@@ -1,6 +1,7 @@
 import requests
 import json
-from coinAPI_service import API_KEY, BASE_URL, coinAPI_service_get_all_assets, coinAPI_get_exchange_rates
+from coinAPI_service import API_KEY, BASE_URL
+from fonctions_app import coinAPI_service_get_all_assets, coinAPI_get_exchange_rates
 
 
 coinAPI_get_exchange_rates()
