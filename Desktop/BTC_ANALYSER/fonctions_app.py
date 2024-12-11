60f276e8bf1ad1c2e45662b0659486df2339c118
api-scraping <bao.francis.nguyen@gmail.com>
2024-12-11 17:02:19 +0100

    ajout fonction save json

--- Desktop/BTC_ANALYSER/fonctions_app.py
+++ Desktop/BTC_ANALYSER/fonctions_app.py
@@ -2,6 +2,7 @@ import requests
 import json
 from coinAPI_service import BASE_URL
 from api_config import API_KEY
+from datetime import date, timedelta
 
     ####Fonctions####
 
@@ -17,22 +18,45 @@ def coinAPI_service_get_all_assets():
     return response
 
 def coinAPI_get_exchange_rates(asset_id_base, asset_id_quote, time_start, time_end, period_id):
-    url = BASE_URL + "v1/exchangerate/"+ asset_id_base + "/" + asset_id_quote + "/history?period_id=" + period_id + "&time_start="+ time_start + "&time_end=" + time_end
-    payload = {}
+    url = BASE_URL + "v1/exchangerate/" + asset_id_base + "/" + asset_id_quote + "/history"
+    params = {
+        "period_id": period_id,
+        "time_start": time_start,
+        "time_end": time_end
+    }
     headers = {
         'Accept': 'application/json',
         'X-CoinAPI-Key': API_KEY
     }
     try:
-        response = requests.request("GET", url, headers=headers, data=payload)
-        print('analyse sur le taux de change ' + asset_id_base + '/' + asset_id_quote + 'sur les 10 premiers jours de 2024')
+        response = requests.get(url, headers=headers, params=params)
+        print(f"Analyse sur le taux de change {asset_id_base}/{asset_id_quote} entre {time_start} et {time_end}")
         if response.status_code == 200:
-            data = json.loads(response.text)
-            # affichez les données
-
+            data = response.json()
+            # Optionnel : Affichage des données pour vérification
             for item in data:
-                print(f"Date : {item['time_period_start'][:10]} Taux : {item['rate_close']}") #taux à la fermeture / fin de journée, on enlève les heures   
+                print(f"Date : {item['time_period_start'][:10]}, Taux : {item['rate_close']}")
+            return data
         else:
             print(f"Erreur {response.status_code}: {response.text}")
+            return None
     except Exception as e:
         print(f"Erreur lors de l'appel API : {e}")
+        return None
+
+
+
+
+def get_json_rates(rates_data):
+    rate_json = []
+    for item in rates_data:
+        rate_json.append({"date": item['time_period_start'][:10], "value": item['rate_close']})
+    return json.dumps(rate_json, indent=4)  # Indenté pour une meilleure lisibilité
+
+    
+
+def save_json_rates(rates_data, filename):
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(get_json_rates(rates_data))
+
+        
