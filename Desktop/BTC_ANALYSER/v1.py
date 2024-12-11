bf6b039bda436d0cc45c52c08050772bfb16f34b
api-scraping <bao.francis.nguyen@gmail.com>
2024-12-11 13:12:07 +0100

    ajout parametres

--- Desktop/BTC_ANALYSER/v1.py
+++ Desktop/BTC_ANALYSER/v1.py
@@ -1,7 +1,8 @@
 import requests
 import json
-from coinAPI_service import API_KEY, BASE_URL
+from coinAPI_service import BASE_URL
+from api_config import API_KEY
 from fonctions_app import coinAPI_service_get_all_assets, coinAPI_get_exchange_rates
 
 
-coinAPI_get_exchange_rates()
+coinAPI_get_exchange_rates(asset_id_base='BTC', asset_id_quote='EUR', time_start='2024-01-01T00:00:00', time_end='2024-01-10T00:00:00', period_id='1DAY')
