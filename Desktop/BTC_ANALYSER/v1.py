1b13e2a87d5397874b450645deb1c1ce0793a633
api-scraping <bao.francis.nguyen@gmail.com>
2024-12-11 22:04:10 +0100

    ajout find_missing_dates

--- Desktop/BTC_ANALYSER/v1.py
+++ Desktop/BTC_ANALYSER/v1.py
@@ -2,9 +2,8 @@ import requests
 import json
 from coinAPI_service import BASE_URL
 from api_config import API_KEY
-from fonctions_app import coinAPI_service_get_all_assets, coinAPI_get_exchange_rates, get_json_rates, save_json_rates
-
-from datetime import date, timedelta
+from fonctions_app import coinAPI_service_get_all_assets, coinAPI_get_exchange_rates, get_json_rates, save_json_rates, find_missing_dates
+from datetime import date, timedelta, datetime
 
 ### affichier la date du jour et la date -10 jours
 
@@ -15,11 +14,11 @@ today = date.today()
 #date d'aujourd'hui
 today_str = today.strftime("%Y-%m-%d")
 
-# date d'y a 10 jours
-delta_10 = today - timedelta(days=10)
+# date d'y a 100 jours
+delta_100 = today - timedelta(days=100)
 
-# date d'y a 10 jours en string
-diff_str = delta_10.strftime("%Y-%m-%d")
+# date d'y a 100 jours en string
+diff_str = delta_100.strftime("%Y-%m-%d")
 
 ### limite de l'appel API en mode gratuit : max 100 appels par jour, max: historique de 100 jours
 
@@ -37,6 +36,8 @@ diff_str = delta_10.strftime("%Y-%m-%d")
 
 asset_id_base = 'BTC'
 asset_id_quote = 'EUR'
+time_start = diff_str
+time_end = today_str
 
 rates = coinAPI_get_exchange_rates(asset_id_base='BTC', asset_id_quote='EUR', time_start=diff_str, time_end= today_str, period_id='1DAY')
 filename = asset_id_base + '_' + asset_id_quote + ".json"
@@ -46,7 +47,13 @@ if rates:
     print(json_rates)
     save_json_rates(rates, filename)
     print(f"Les taux de change ont été sauvegardés dans le fichier {filename}")
+    #print(f"Taux récupérés pour {time_start} à {time_end}: {rates}")
+
 else:
     print("Erreur lors de la récupération des taux de change")
 
+start_date = datetime.strptime(time_start, "%Y-%m-%d")
+end_date = datetime.strptime(time_end, "%Y-%m-%d")
+missing_dates = find_missing_dates(start_date, end_date, rates)
+print("Dates manquantes :", missing_dates)
 
