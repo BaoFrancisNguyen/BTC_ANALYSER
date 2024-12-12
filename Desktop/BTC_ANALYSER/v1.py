642f43b30bee3e51395dbd1724f2c17d9dc45d2c
api-scraping <bao.francis.nguyen@gmail.com>
2024-12-12 20:08:46 +0100

    intervals ok

--- Desktop/BTC_ANALYSER/v1.py
+++ Desktop/BTC_ANALYSER/v1.py
@@ -2,7 +2,7 @@ import requests
 import json
 from coinAPI_service import BASE_URL
 from api_config import API_KEY
-from fonctions_app import coinAPI_service_get_all_assets, coinAPI_get_exchange_rates, get_json_rates, save_json_rates, find_missing_dates, load_json_data_from_file
+from fonctions_app import coinAPI_service_get_all_assets, coinAPI_get_exchange_rates, get_json_rates, save_json_rates, find_missing_dates, load_json_data_from_file, get_dates_interval
 from datetime import date, timedelta, datetime
 from os import path
 from datetime import date, timedelta, datetime
@@ -19,6 +19,7 @@ filename = f"{asset_id_base}_{asset_id_quote}.json"
 
 # Charger ou créer le fichier JSON
 if path.exists(filename):
+    #le fichier json existe
     print(f"Chargement des données depuis {filename}...")
     json_rates = load_json_data_from_file(filename)
     rates = json.loads(json_rates)
@@ -34,6 +35,7 @@ if path.exists(filename):
             print(f"Dates manquantes : {missing_dates}")
     else:
         print("Aucune donnée trouvée dans le fichier.")
+#si le fichier json n'existe pas
 else:
     print(f"Aucun fichier trouvé. Création de {filename}...")
     rates = coinAPI_get_exchange_rates(asset_id_base, asset_id_quote, diff_str, today_str, period_id='1DAY')
@@ -42,4 +44,27 @@ else:
         print(f"Données sauvegardées dans {filename}.")
     else:
         print("Erreur : Impossible de récupérer les données via l'API.")
+
+
+
+# Les dates au format str
+date_start_str = "2021-01-01"
+date_end_str = "2021-01-10"
+
+# Conversion en objets datetime.date
+date_start = datetime.strptime(date_start_str, "%Y-%m-%d").date()
+date_end = datetime.strptime(date_end_str, "%Y-%m-%d").date()
+
+# Appel de la fonction
+intervals = get_dates_interval(date_start, date_end)
+print(intervals)
+
+# si le fichier json existe:
+# comparer le date_star avec le saved_data_date_start
+# comparer le date_end avec le saved_data_date_end
+# [date_start / saved_data_date_start] [date_end / saved_data_date_end]
+
+#si le fichier json n'existe pas:
+# créer le fichier json
+# [date_start / date_end]
 
