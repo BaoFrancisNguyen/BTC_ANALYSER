1152cee64c64700d9f644bc43a53004508399d04
api-scraping <bao.francis.nguyen@gmail.com>
2024-12-18 16:23:02 +0100

    ajout matrice

--- Desktop/game_of_life/game_of_life.py
+++ Desktop/game_of_life/game_of_life.py
@@ -9,6 +9,19 @@ frame = np.array([[0, 0, 0, 0, 0, 0, 0],
                   [0, 0, 0, 0, 0, 0, 0],
                   [0, 0, 0, 0, 0, 0, 0]])
 
+
+### Index de la frame:
+
+'''Frame actuelle :
+    0 1 2 3 4 5 6
+0 | 0 0 0 0 0 0 0
+1 | 0 0 0 0 0 0 0
+2 | 0 0 0 0 0 0 0
+3 | 0 0 1 1 1 0 0
+4 | 0 0 0 0 0 0 0
+5 | 0 0 0 0 0 0 0
+6 | 0 0 0 0 0 0 0'''
+
 # fonction pour calculer le nombre de voisins
 def compute_number_neighbors(paded_frame, index_row, index_col):
     """
@@ -17,14 +30,28 @@ def compute_number_neighbors(paded_frame, index_row, index_col):
     """
     # Initialiser le nombre de voisins à 0
     number_neighbors = 0
-    # parcourir les cellules voisines
-    for row in range(index_row - 1, index_row + 2):
+    # on cherche les cellules voisines de la cellule actuelle (contenant les 1) ---> on parcourt les cellules voisines
+     # on ne doit pas inclure la cellule actuelle
+
+        # position de la cellule actuelle : index_row, index_col
+        # position des cellules voisines : (index_row - 1, index_col - 1), (index_row - 1, index_col), (index_row - 1, index_col + 1)
+    # vérifier si la cellule est vivante et différente de la cellule actuelle
+    # if not (row == index_row and col == index_col): # on ne vérifie pas la cellule actuelle
+    # ajouter la valeur de la cellule à number_neighbors
+
+    
+    
+    for row in range(index_row - 1, index_row + 2): #attention à l'exclusion de la valeur de fin / pas index_row + 1 mais index_row + 2
         # parcourir les colonnes voisines
-        for col in range(index_col - 1, index_col + 2):
+        for col in range(index_col - 1, index_col + 2): #attention à l'exclusion de la valeur de fin / pas index_col + 1 mais index_col + 2
+
             # vérifier si la cellule est vivante et différente de la cellule actuelle
+            # La boucle parcourt toutes les cases autour de la cellule centrale, y compris la cellule centrale elle-même.
+            # On doit ajouter if not pour ignorer la cellule centrale.
             if not (row == index_row and col == index_col):
                 # ajouter la valeur de la cellule à number_neighbors
                 number_neighbors += paded_frame[row, col]
+                
     return number_neighbors
 
 
