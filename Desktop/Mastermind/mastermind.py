e7406ffdfb1b400cd7602b69ad9d26bbf465d8c1
api-scraping <bao.francis.nguyen@gmail.com>
2024-12-18 10:01:00 +0100

    add get_player_combinaison

--- Desktop/Mastermind/mastermind.py
+++ Desktop/Mastermind/mastermind.py
@@ -6,23 +6,31 @@ import random
 
 # Affiche un message d'accueil avec les règles du jeu
 def show_game_instructions():
-    print("Bienvenue dans Mastermind")
+    print("Mastermind")
     print("Tu dois deviner une combinaison de 4 couleurs")
     print("Les couleurs possibles sont : Rouge, Bleu, Vert, Jaune, Orange, Violet")
-    print("Tu as 10 essais pour réussir. Bonne chance\n")
 
 # Générer un code secret aléatoire
 
 def generate_secret_code():
 
     # liste des couleurs possibles
-    couleurs = ["Rouge", "Bleu", "Vert", "Jaune", "Orange", "Violet"]
+    color_options = ["Rouge", "Bleu", "Vert", "Jaune", "Orange", "Violet"]
     # création d'une liste vide pour stocker le code secret
-    code_secret = []
+    generated_code = []
     # Boucle pour générer 4 couleurs
     # for_ pour indiquer que la variable n'est pas utilisée
     for _ in range(4): #génère une séquence de 4 nombres
         # Ajout d'une couleur aléatoire à la liste
-        code_secret.append(random.choice(couleurs))
+        generated_code.append(random.choice(color_options))
         # Retourne le code secret
-    return code_secret
+    return generated_code
+
+# combinaison du joueur / input
+def get_player_combination():
+    # combinaison du joueur
+    user_input = input("Tapez votre combinaison (couleurs séparées par des espaces, attention à la casse) : ")
+    # split() pour séparer les couleurs et les stocker dans une liste
+    player_combination = user_input.split()
+    # retourne la combinaison du joueur
+    return player_combination
